e39b1dca19a33976c13755a95cb2964461bc5402
metacoding <getinthere@naver.com>
2022-07-14 22:55:28 +0900

    initial commit

---
+++ src/main/java/site/metacoding/junitproject/JunitProjectApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.junitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/junitproject/JunitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.junitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

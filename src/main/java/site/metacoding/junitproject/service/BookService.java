f14fe9b826ab73fc1e5df2863390ae602c68c4be
metacoding <getinthere@naver.com>
2022-07-31 23:11:46 +0900

    북서비스 책등록 완료

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -1,8 +1,32 @@
 package site.metacoding.junitproject.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.junitproject.domain.Book;
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.web.dto.BookRespDto;
+import site.metacoding.junitproject.web.dto.BookSaveReqDto;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
 
+    private final BookRepository bookRepository;
+
+    // 1. 책등록
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookRespDto 책등록하기(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookRespDto().toDto(bookPS);
+    }
+
+    // 2. 책목록보기
+
+    // 3. 책한건보기
+
+    // 4. 책삭제
+
+    // 5. 책수정
 }
--- src/main/java/site/metacoding/junitproject/web/dto/BookRespDto.java
+++ src/main/java/site/metacoding/junitproject/web/dto/BookRespDto.java
@@ -1,5 +1,21 @@
 package site.metacoding.junitproject.web.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.metacoding.junitproject.domain.Book;
+
+@NoArgsConstructor
+@Getter
 public class BookRespDto {
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookRespDto toDto(Book bookPS) {
+        this.id = bookPS.getId();
+        this.title = bookPS.getTitle();
+        this.author = bookPS.getAuthor();
+        return this;
+    }
 
 }

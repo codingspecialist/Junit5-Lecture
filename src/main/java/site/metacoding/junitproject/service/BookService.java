a24fe0c136670cd87a440994333a037a9c1e6d5a
metacoding <getinthere@naver.com>
2022-08-11 08:13:55 +0900

    북서비스 책한건보기, 책삭제하기, 책수정하기 완료

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -1,6 +1,7 @@
 package site.metacoding.junitproject.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -33,8 +34,30 @@ public class BookService {
     }
 
     // 3. 책한건보기
+    public BookRespDto 책한건보기(Long id) {
+        Optional<Book> bookOP = bookRepository.findById(id);
+        if (bookOP.isPresent()) { // 찾았다면
+            return new BookRespDto().toDto(bookOP.get());
+        } else {
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
+    }
 
-    // 4. 책삭제
+    // 4. 책삭제하기
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void 책삭제하기(Long id) { // 4
+        bookRepository.deleteById(id); // 1,2,3
+    }
 
-    // 5. 책수정
+    // 5. 책수정하기
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void 책수정하기(Long id, BookSaveReqDto dto) { // id, title, author
+        Optional<Book> bookOP = bookRepository.findById(id);
+        if (bookOP.isPresent()) {
+            Book bookPS = bookOP.get();
+            bookPS.update(dto.getTitle(), dto.getAuthor());
+        } else {
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
+    } // 메서드 종료시에 더티체킹(flush)으로 update 됩니다.
 }
--- src/main/java/site/metacoding/junitproject/web/dto/BookSaveReqDto.java
+++ src/main/java/site/metacoding/junitproject/web/dto/BookSaveReqDto.java
@@ -1,8 +1,10 @@
 package site.metacoding.junitproject.web.dto;
 
+import lombok.Getter;
 import lombok.Setter;
 import site.metacoding.junitproject.domain.Book;
 
+@Getter
 @Setter // Controller에서 Setter가 호출되면서 Dto에 값이 채워짐.
 public class BookSaveReqDto {
     private String title;

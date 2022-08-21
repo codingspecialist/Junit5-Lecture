d11c7bcaeffe7f5bc3f6f248c938edfd60072c30
metacoding <getinthere@naver.com>
2022-08-21 13:33:10 +0900

    서비스 레이어 - 책삭제하기(생략), 책수정하기 테스트 완료

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -69,13 +69,15 @@ public class BookService {
 
     // 5. 책수정하기
     @Transactional(rollbackFor = RuntimeException.class)
-    public void 책수정하기(Long id, BookSaveReqDto dto) { // id, title, author
+    public BookRespDto 책수정하기(Long id, BookSaveReqDto dto) { // id, title, author
         Optional<Book> bookOP = bookRepository.findById(id);
         if (bookOP.isPresent()) {
             Book bookPS = bookOP.get();
-            bookPS.update(dto.getTitle(), dto.getAuthor());
+            bookPS.update(dto.getTitle(), dto.getAuthor()); // update 메서드 테스트를 못해본다.
+            return bookPS.toDto();
         } else {
             throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
         }
+
     } // 메서드 종료시에 더티체킹(flush)으로 update 됩니다.
 }
--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
@@ -87,5 +88,45 @@ public class BookServiceTest {
         assertThat(bookRespDtoList.get(0).getAuthor()).isEqualTo("메타코딩");
         assertThat(bookRespDtoList.get(1).getTitle()).isEqualTo("spring강의");
         assertThat(bookRespDtoList.get(1).getAuthor()).isEqualTo("겟인데어");
+    }
+
+    @Test
+    public void 책한건보기_테스트() {
+        // given
+        Long id = 1L;
+
+        // stub
+        Book book = new Book(1L, "junit강의", "메타코딩");
+        Optional<Book> bookOP = Optional.of(book);
+        when(bookRepository.findById(id)).thenReturn(bookOP);
+
+        // when
+        BookRespDto bookRespDto = bookService.책한건보기(id);
+
+        // then
+        assertThat(bookRespDto.getTitle()).isEqualTo(book.getTitle());
+        assertThat(bookRespDto.getAuthor()).isEqualTo(book.getAuthor());
+    }
+
+    @Test
+    public void 책수정하기_테스트() {
+        // given
+        Long id = 1L;
+        BookSaveReqDto dto = new BookSaveReqDto();
+        dto.setTitle("spring강의"); // spring강의
+        dto.setAuthor("겟인데어"); // 겟인데어
+
+        // stub
+        Book book = new Book(1L, "junit강의", "메타코딩");
+        Optional<Book> bookOP = Optional.of(book);
+        when(bookRepository.findById(id)).thenReturn(bookOP);
+
+        // when
+        BookRespDto bookRespDto = bookService.책수정하기(id, dto);
+
+        // then
+        assertThat(bookRespDto.getTitle()).isEqualTo(dto.getTitle());
+        assertThat(bookRespDto.getAuthor()).isEqualTo(dto.getAuthor());
+
     }
 }

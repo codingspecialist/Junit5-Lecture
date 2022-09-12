40a71240cec7f25db018a69a5fa99eda7e3d5075
metacoding <getinthere@naver.com>
2022-09-12 12:16:36 +0900

    스프링 컨트롤러 레이어 책수정하기 완료

--- src/main/java/site/metacoding/junitproject/web/BookApiController.java
+++ src/main/java/site/metacoding/junitproject/web/BookApiController.java
@@ -10,8 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -65,21 +68,41 @@ public class BookApiController { // 컴포지션 = has 관계
     public ResponseEntity<?> getBookList() {
         BookListRespDto bookListRespDto = bookService.책목록보기();
         return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 목록보기 성공").body(bookListRespDto).build(),
-                HttpStatus.OK); // 200 = ok;
+                HttpStatus.OK); // 200 = ok
     }
 
     // 3. 책한건보기
-    public ResponseEntity<?> getBookOne() {
-        return null;
+    @GetMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> getBookOne(@PathVariable Long id) {
+        BookRespDto bookRespDto = bookService.책한건보기(id);
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 한건보기 성공").body(bookRespDto).build(),
+                HttpStatus.OK); // 200 = ok
     }
 
     // 4. 책삭제하기
-    public ResponseEntity<?> deleteBook() {
-        return null;
+    @DeleteMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+        bookService.책삭제하기(id);
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 삭제하기 성공").body(null).build(),
+                HttpStatus.OK); // 200 = ok
     }
 
     // 5. 책수정하기
-    public ResponseEntity<?> updateBook() {
-        return null;
+    @PutMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> updateBook(@PathVariable Long id, @RequestBody @Valid BookSaveReqDto bookSaveReqDto,
+            BindingResult bindingResult) {
+
+        // AOP 처리하는 게 좋음!!
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                errorMap.put(fe.getField(), fe.getDefaultMessage());
+            }
+            throw new RuntimeException(errorMap.toString());
+        }
+
+        BookRespDto bookRespDto = bookService.책수정하기(id, bookSaveReqDto);
+        return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 수정하기 성공").body(bookRespDto).build(),
+                HttpStatus.OK); // 200 = ok
     }
 }
